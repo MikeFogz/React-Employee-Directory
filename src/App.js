e84c745bbe32e1b826188ceea4436d0517bc167e
MikeFogz <MikeFogz@gmail.com>
2021-02-25 23:06:31 -0800

    Added two npm packages & restarted App.js from scratch.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactTable from "react-table"; 
-// import 'react-table/react-table.css'
 
-export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      users: [],
-      loading:true
-    }
-  }
-  async getUsersData(){
-    const res = await axios.get('https://randomuser.me/api/')
-    console.log(res.data)
-    this.setState({loading:false, users: res.data})
-  }
-  componentDidMount(){
-    this.getUsersData()
-  }
-  render() {
-    const columns = [{  
-      Header: 'ID',  
-      accessor: 'id',
-     }
-     ,{  
-      Header: 'Name',  
-      accessor: 'name' ,
-      }
-     
-     ,{  
-     Header: 'City',  
-     accessor: 'city' ,
-     }
-     ,{  
-     Header: 'Phone',  
-     accessor: 'phone',
-     },
-     {  
-      Header: 'Email',  
-      accessor: 'email',
-      },
-      {  
-        Header: 'Website',  
-        accessor: 'website',
-        }
-  ]
+
+export default function App() {
+  const [data, setData] = useState([])
+  const [q, setQ] = useState()
+
     return (
       <ReactTable  
       data={this.state.users}  
       columns={columns}  
    />
-    )
-  }
+    );
 }
