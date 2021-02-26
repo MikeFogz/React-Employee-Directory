cade05bc3349988d75ef48e2adcf66a026504da0
MikeFogz <MikeFogz@gmail.com>
2021-02-25 23:23:16 -0800

    Added api fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactTable from "react-table"; 
-
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
 export default function App() {
-  const [data, setData] = useState([])
-  const [q, setQ] = useState()
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState();
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+    .then(response => response.json())
+    .then(json => setData(json));
+  }, []);
 
     return (
       <ReactTable  
